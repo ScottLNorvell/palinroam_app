113ff6ee9e915ca74d8b25919210c31c24e383d6
Camila Cano <camilacanog@gmail.com>
2013-10-08 10:22:16 -0400

    add half a method on palindroma controller

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,4 +2,23 @@ class PalindromesController < ApplicationController
   def index
   	@palindromes = Palindrome.all 
   end
+
+  def scrape
+  	url = params[:url]
+  	max_length = 100
+
+  	response = HTTParty.get(url)
+
+  	text = response.body.downcase.split(' ')
+
+  	site =  Site.find_by_url(url) || Site.create(url: url)
+
+  	1.upto(text.length - max_length) do |index_a|
+  		(index_a+1).upto(index_a+max_length) do |index_b|
+  			tst_phrase = text[index_a..index_b].join()
+
+  			if test_phrase[0]  
+  		end
+  	end
+  end	
 end
